a794df76e342c21f00f2f7b079a5fd2c8aed77f8
202410401@fit.edu.ph <202410401@fit.edu.ph>
2026-03-20 14:35:38 +0800

    created testing.py and added comments to main.py

--- mainProgram.py
+++ mainProgram.py
@@ -1,6 +1,12 @@
-# PROGRAM SPECIFICATIONS
-
+# PROGRAM SPECIFICATIONS:
 # Good layout (GUI)
 # Data persistence: File Handling (Binary file) w/ CRUD functionality
 # Exception handling
 # Collections
+# KEY CONCEPT / PROGRAM BASIS: Expenses Tracker
+
+# CRUD system outline
+
+# Display menu func
+
+# Main function
