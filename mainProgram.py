1a5a4c3bb141648c8b2dec764da6a9b27dfbdd3e
202410401@fit.edu.ph <202410401@fit.edu.ph>
2026-03-24 20:15:04 +0800

    cleaned repo

--- mainProgram.py
+++
@@ -1,7 +0,0 @@
-# PROGRAM SPECIFICATIONS:
-# Good layout (GUI)
-# Data persistence: File Handling (Binary file) w/ CRUD functionality
-# Exception handling
-# Collections
-# KEY CONCEPT / PROGRAM BASIS: Expenses Tracker
-
